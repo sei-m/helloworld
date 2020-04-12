9b0e4ee92e4e83ee1b43f43660c0020ccdafd420
sei-m <seiichimori@i.softbank.jp>
2020-04-12 14:03:23 +0900

    Initial Commit

---
+++ hello.world/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hello.world
+//
+//  Created by USER on 2020/04/12.
+//  Copyright © 2020 test.seimo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
